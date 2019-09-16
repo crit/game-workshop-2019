3994062d087251039618eb19c88cba4a735b17d5
Crit Russell <critrussell@gmail.com>
2019-09-16 13:26:48 -0600

    game loop & render

--- engine/game.js
+++ engine/game.js
@@ -9,13 +9,29 @@ class Game {
     this.height = element.height
     this.stage = element.getContext('2d')
 
-    // Temporary Hello World
-    this.stage.fillStyle = '#ecf0f1'
-    this.stage.textAlign = 'center'
-    this.stage.font = '24px sans-serif'
-    this.stage.fillText('Stage Set And Ready To Work!', this.width / 2, this.height / 2)
-    this.stage.fillStyle = '#95a5a6'
-    this.stage.font = '16px sans-serif'
-    this.stage.fillText('If everything loaded correctly, we can get started.', this.width / 2, this.height / 2 + 32)
+    assets.download(() => this.loop())
+  }
+
+  loop() {
+    this.entities.forEach((entity, i) => {
+      // todo: movement, animation, etc
+    })
+
+    this.render()
+
+    window.requestAnimationFrame(() => this.loop())
+  }
+
+  render() {
+    const updates = []
+
+    this.entities.forEach(entity => {
+      if (entity.has(['active', 'render'])) updates.push(entity.com('renderer'))
+    })
+
+    if (!updates.length) return
+
+    this.stage.clearRect(0, 0, this.width, this.height)
+    updates.forEach(update => update.run(this.stage))
   }
 }
